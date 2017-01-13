d4d74af21a9b11a144b748d8f8290313ea0b0066
lei110408 <lei110408@gmail.com>
2017-01-12 20:09:32 -0800

    Add albumLIst and header

--- index.ios.js
+++ index.ios.js
@@ -4,13 +4,19 @@
 //import a library to help create a component
 
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry,View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 //create a component
 
 
 const App = () => (
-    <Text>Some Text</Text>
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+
 );
 
 
---
+++ src/components/AlbumList.js
@@ -0,0 +1,17 @@
+/**
+ * Created by leichen on 2017/1/12.
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumList = () => {
+    return (
+        <View>
+            <Text> Album List!!!</Text>
+        </View>
+    );
+};
+
+
+export default AlbumList;
