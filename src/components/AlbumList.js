557d7c794e75c47e44a63377861a331e993c47d7
lei110408 <lei110408@gmail.com>
2017-01-14 00:06:44 -0800

    Added card and card section
    Added card and card section, the main layout of the app has been
    finished.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,7 +3,7 @@
  */
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -23,9 +23,9 @@ class AlbumList extends Component {
     render() {
         console.log(this.state)
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 
